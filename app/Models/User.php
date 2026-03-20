e81002bbd5e77740521f8bfa80fd2614d74e7403
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-03-20 18:51:52 +0100

    feat: ajouter la configuration de jwt authentification.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
 
@@ -42,5 +43,17 @@ class User extends Authenticatable
     public function blockedAccounts(): HasMany
     {
         return $this->hasMany(BlockedAccount::class, 'admin_id');
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [
+            'role' => $this->role
+        ];
     }
 }
